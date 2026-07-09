9eb9c1df3641ccabddd5140b2c02495cbf8ccf22
Robert <kopipes@gmail.com>
2026-07-09 21:08:01 +0700

    fix: track prisma migrations in git, remove from .gitignore

---
+++ prisma/migrations/20260709140451_add_note_userid/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Note" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL DEFAULT 'Catatan Baru',
+    "content" TEXT NOT NULL DEFAULT '',
+    "summary" TEXT,
+    "userId" TEXT NOT NULL DEFAULT '',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Note" ("content", "createdAt", "id", "summary", "title", "updatedAt") SELECT "content", "createdAt", "id", "summary", "title", "updatedAt" FROM "Note";
+DROP TABLE "Note";
+ALTER TABLE "new_Note" RENAME TO "Note";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
